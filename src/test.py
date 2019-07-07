b2e9c3db3100b36ec6ee92137d6db16b2f6bba4f
Ansar <dfqgth400@gmail.com>
2019-07-07 19:37:29 +0600

    Defining tests

--- src/test.py
+++ src/test.py
@@ -1,9 +1,47 @@
 # Test module
-# Run any test module you want
+# Run any test you want
+# Test list:
+# 1) Create objects with different inputs
+# 2) Accessing any matrix cell, writing & reading
+# 3) Inputing matrix data by console
+# 4) Randoming values in matrix
+# 5) Summation
+# 6) Multiplication (by constant or matrix)
+# 7) Finding matrix determinant
+# 8) Finding inverse of matrix
 
 from src.matrix import Matrix
 
 temp = Matrix(3, 3)
 
+
 def test_createObjects():
-    
+    pass
+
+
+def test_accessData():
+    pass
+
+
+def test_inputData():
+    pass
+
+
+def test_randomizeData():
+    pass
+
+
+def test_summation():
+    pass
+
+
+def test_multiplication():
+    pass
+
+
+def test_determinant():
+    pass
+
+
+def test_inverse():
+    pass
